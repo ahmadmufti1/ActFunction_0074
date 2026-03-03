bfe0e3cac57b808093aa96c04784bf583d766153
ahmadmufti1 <159124018+ahmadmufti1@users.noreply.github.com>
2026-03-03 15:41:54 +0700

    Update Praktikum2.cpp

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
 using namespace std;
-
+//Deklarasi Variable Global
 float p,l;
 
-void Input()
-{
-    cout << "Masukkan Panjang : ";
-    cin >> p;
-    cout << "Masukkan Lebar : "; 
-    cin >> l;
-}
 
-float LuasPersegi(float x, float y){
-    return x*y;
-}
-
-int Jumlah(int a, int b, int c){
-    return a+b+c;
-}
-
-void Output()
-{
-    cout << "Hasilnya = " << LuasPersegi(p,l) <<endl;
-}
-
-int main()
-{
-    Input();
-    Output();
-    cout << "Hasil OEnjumlahan = "<< Jumlah(5,6,9);
-}
 
 
