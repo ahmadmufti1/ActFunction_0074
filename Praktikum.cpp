0e63d9e7b10c43c06f52bad3804235f464ebd71e
ahmadmufti1 <159124018+ahmadmufti1@users.noreply.github.com>
2026-03-03 15:33:36 +0700

    Update Praktikum.cpp

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Deklarasi Variable Global
+float p,l;
+//Deklarasi Prosedur
+void Input()
+{
+    cout << "Masukkan Panjan : ";
+    cin >> p;
+    cout << "Masukkan Lebar : "; 
+    cin >> l;
+}
+
+//Proses
+float LuasPersegi(){
+    return p*l;
+}
+
+//Hasil Proses
+void Output()
+{
+    cout << "Hasilnya = " << LuasPersegi();
+}
+
 //Menjalankan Program
 int main()
 {
