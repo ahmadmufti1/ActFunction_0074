f12a45afdb758b545e5ca299379ada995eff0ef5
ahmadmufti1 <159124018+ahmadmufti1@users.noreply.github.com>
2026-03-03 15:44:00 +0700

    Update Praktikum2.cpp

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-
+//Deklarasi Variable Global
 float p,l;
 
+//Implementasi Prosedur dan Fungsi
 void Input()
 {
     cout << "Masukkan Panjang : ";
@@ -24,11 +25,13 @@ void Output()
     cout << "Hasilnya = " << LuasPersegi(p,l) <<endl;
 }
 
+//Program Utama
 int main()
 {
     Input();
     Output();
     cout << "Hasil OEnjumlahan = "<< Jumlah(5,6,9);
 }
+
 
 
