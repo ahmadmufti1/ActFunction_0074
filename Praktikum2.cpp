e4145041870d8af980ce759f187b8a3fc82a64be
ahmadmufti1 <159124018+ahmadmufti1@users.noreply.github.com>
2026-03-03 15:42:52 +0700

    Update Praktikum2.cpp

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -1,11 +1,34 @@
+#include <iostream>
+using namespace std;
 
-//Deklarasi Prosedur
+float p,l;
+
 void Input()
 {
     cout << "Masukkan Panjang : ";
     cin >> p;
     cout << "Masukkan Lebar : "; 
     cin >> l;
+}
+
+float LuasPersegi(float x, float y){
+    return x*y;
+}
+
+int Jumlah(int a, int b, int c){
+    return a+b+c;
+}
+
+void Output()
+{
+    cout << "Hasilnya = " << LuasPersegi(p,l) <<endl;
+}
+
+int main()
+{
+    Input();
+    Output();
+    cout << "Hasil OEnjumlahan = "<< Jumlah(5,6,9);
 }
 
 
